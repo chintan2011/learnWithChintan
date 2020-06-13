ee516a82484989b515927833a54b4dc27099599e
Chintan Savalia <chintan2011@gmail.com>
2020-06-13 09:57:34 -0400

    adding content and modified model

--- blog/models.py
+++ blog/models.py
@@ -19,12 +19,12 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     tech_stack = models.CharField(max_length=200, default='')
-    github_url = models.URLField(max_length=200, default='n/a')
+    github_url = models.URLField(max_length=200, default='n/a', blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    tech_image = models.ImageField(upload_to="tech_image", blank=True)
+    screen_shot = models.ImageField(upload_to="tech_image", blank=True)
     #image_urls = models.TextField()
     images = ListCharField(
         base_field=CharField(max_length=100),
