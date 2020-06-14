9d7a4af02214d17bfa6aa686f4ad675a3a8c7a74
Chintan Savalia <chintan2011@gmail.com>
2020-06-14 12:32:06 -0400

    master update

--- learnWithChintan/settings.py
+++ learnWithChintan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['chintan2011.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
