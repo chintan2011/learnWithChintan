ba07080085e81bbc3c04c331cc1c18e10edca643
Chintan Savalia <chintan2011@gmail.com>
2020-07-12 17:50:09 -0400

    heroku update

--- learnWithChintan/settings.py
+++ learnWithChintan/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Heroku things added by me
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#added by me for heroku
+django_heroku.settings(locals())
